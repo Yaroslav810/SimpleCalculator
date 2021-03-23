54242269541e3fcca05d6805dad7b66dfb6a0e3c
Yaroslav Kuzmin <Yaroslav.Kuzmin.01@yandex.ru>
2021-03-23 15:11:06 +0300

    Добавьте файлы проекта.

---
+++ SimpleCalculator/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число 1: ");
+            int firstNumber = 0;
+            if (!int.TryParse(Console.ReadLine(), out firstNumber))
+            {
+                Console.WriteLine("Ошибка! Некорректное первое число!");
+                return;
+            }
+
+            Console.WriteLine("Введите число 2: ");
+            int secondNumber = 0;
+            if (!int.TryParse(Console.ReadLine(), out secondNumber))
+            {
+                Console.WriteLine("Ошибка! Некорректное второе число!");
+                return;
+            }
+
+            Console.WriteLine("Введите операцию: ");
+            char operation = char.Parse(Console.ReadLine());
+            switch (operation)
+            {
+                case '+':
+                    Console.WriteLine(Add(firstNumber, secondNumber));
+                    break;
+                case '-':
+                    Console.WriteLine(Sub(firstNumber, secondNumber));
+                    break;
+                case '*':
+                    Console.WriteLine(Mul(firstNumber, secondNumber));
+                    break;
+                case '/':
+                    Console.WriteLine(Div(firstNumber, secondNumber));
+                    break;
+                default:
+                    Console.WriteLine("Ошибка! Некорректная операция");
+                    break;
+            }
+        }
+
+        public static int Add(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public static int Sub(int firstNumber, int secondNumber)
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public static int Mul(int firstNumber, int secondNumber)
+        {
+            return firstNumber * secondNumber;
+        }
+
+        public static int Div(int firstNumber, int secondNumber)
+        {
+            return firstNumber / secondNumber;
+        }
+    }
+}
